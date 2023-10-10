74bc05fb5ad1cafd8f9cd32969364851f8b84387
Sophian-Asaidi <132987804+Sophian-Asaidi@users.noreply.github.com>
2023-10-10 08:21:41 +0200

    arborescence
    arborescence du projet + PDO

---
+++ site location/include/bdd.inc.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecurie";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+}
+?>
