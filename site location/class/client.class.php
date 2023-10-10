7fe927b239bd8c7a013a442220d9a1772df2be13
Sophian-Asaidi <132987804+Sophian-Asaidi@users.noreply.github.com>
2023-10-10 11:52:11 +0200

    classe client + type_bien
    finition de la class client et début de la class type_bien

--- site location/class/client.class.php
+++ site location/class/client.class.php
@@ -14,12 +14,16 @@ class client {
     private $status_client;
     private $valid_client;
 
-    public function __construct($id_client, $nom_client, $prenom_client, 
-            $cop_client, $vil_client, $mail_client, $pass_client, $status_client, 
-            $valid_client) {
-        
-        
-        
+    public function __construct($id, $nom, $prenom, $cop, $vil, $mail, $pass, $status, $valid) {
+        $this->id_client = $id;
+        $this->nom_client = $nom;
+        $this->prenom_client = $prenom;
+        $this->cop_client = $cop;
+        $this->vil_client = $vil;
+        $this->mail_client = $mail;
+        $this->pass_client = $pass;
+        $this->status_client = $status;
+        $this->valid_client = $valid;
     }
 
     public function getCid() {
@@ -57,5 +61,70 @@ class client {
     public function getCvalid() {
         return $this->valid_client;
     }
+
+    public function getAll() {
+
+        $sql = "SELECT * FROM client";
+        $stmt = $con->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getOne($id) {
+
+        $data = [':id' => $id];
+        $sql = "SELECT * FROM client WHERE id_client = :id";
+        $stmt = $con->prepare($sql);
+        $stmt->execute($data);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    public function insertC($nom, $prenom, $cop, $vil, $mail, $pass, $status, $valid) {
+        require_once '../include/bdd.inc.php';
+
+        $data = [
+            ':nom' => $nom,
+            ':pre' => $prenom,
+            ':cop' => $cop,
+            ':vil' => $vil,
+            ':mail' => $mail,
+            ':pass' => $pass,
+            ':status' => $status,
+            ':valid' => $valid
+        ];
+
+        $sql = "INSERT INTO client (nom_client, prenom_client, cop_client, vil_client, mail_client, pass_client, status_client, valid_client) VALUES (:nom, :pre, :cop, :vil, :mail, :pass, :status, :valid)";
+        $stmt = $con->prepare($sql);
+        $stmt->execute($data);
+    }
+    
+    public function updateC($id, $nom, $prenom, $cop, $vil, $mail, $pass, $status, $valid) {
+        require_once '../include/bdd.inc.php';
+
+        $data = [
+            ':id' => $id,
+            ':nom' => $nom,
+            ':pre' => $prenom,
+            ':cop' => $cop,
+            ':vil' => $vil,
+            ':mail' => $mail,
+            ':pass' => $pass,
+            ':status' => $status,
+            ':valid' => $valid
+        ];
+
+        $sql = "UPDATE client SET nom_client = :nom, prenom_client = :pre, cop_client = :cop, vil_client = :vil, mail_client = :mail, pass_client = :pass, status_client = :status, valid_client = :valid WHERE id_client = :id";
+        $stmt = $con->prepare($sql);
+        $stmt->execute($data);
+    }
+    
+    public function delete($id) {
+
+        $data = [':id' => $id];
+        $sql = "DELETE FROM client WHERE id_client = :id";
+        $stmt = $con->prepare($sql);
+        $stmt->execute($data);
+    }
+    
+    
 
 }
---
+++ site location/class/type_bien.class.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../include/bdd.inc.php';
+
+class type_bien {
+
+
+}
