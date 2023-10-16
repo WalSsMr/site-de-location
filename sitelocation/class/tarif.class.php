9843a27af9f8a22e849bb1bf6a6369813345c826
Sophian-Asaidi <132987804+Sophian-Asaidi@users.noreply.github.com>
2023-10-16 17:45:54 +0200

    tarif
    class tarif fini + réflexion sur la gestion des class

--- sitelocation/class/tarif.class.php
+++ sitelocation/class/tarif.class.php
@@ -17,12 +17,43 @@ class tarif{
         $this->id_bien = $id_bien;
     }
     
-    public function getTarif($id) {
+    public function getTarifId($id) {
         $con = new PDO('mysql:host=localhost;dbname=rentfr', 'root', '');
         $sql = "SELECT * FROM tarif WHERE id_tarif = :id";
         $stmt = $con->prepare($sql);
         $stmt->execute([':id' => $id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
+    public static function insertTarif($data) {
+        $con = new PDO('mysql:host=localhost;dbname=rentfr', 'root', '');
+        $sql = "INSERT INTO tarif (dad_tarif, daf_tarif, prix_loc, id_bien) VALUES (:dad, :daf, :prix, :idBien)";
+        $stmt = $con->prepare($sql);
+        $stmt->execute($data);
+    }
+    
+    public static function getAllTarif() {
+        $con = new PDO('mysql:host=localhost;dbname=rentfr', 'root', '');
+        $sql = "SELECT * FROM tarif";
+        $stmt = $con->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    // mettre à jour un tarif
+    public static function updateTarif($id, $data) {
+        $con = new PDO('mysql:host=localhost;dbname=rentfr', 'root', '');
+        $data[':id'] = $id;
+        $sql = "UPDATE tarif SET dad_tarif = :dad, daf_tarif = :daf, prix_loc = :prix, id_bien = :idBien WHERE id_tarif = :id";
+        $stmt = $con->prepare($sql);
+        $stmt->execute($data);
+    }
+
+    // supprimer un tarif avec l' ID
+    public static function deleteTarif($id) {
+        $con = new PDO('mysql:host=localhost;dbname=rentfr', 'root', '');
+        $sql = "DELETE FROM tarif WHERE id_tarif = :id";
+        $stmt = $con->prepare($sql);
+        $stmt->execute([':id' => $id]);
     }
     
     
